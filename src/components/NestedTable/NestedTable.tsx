be69a8743f25bdac8783ef92e458671d7c91c5b5
Waiz Wafiq <mwaizwafiq@gmail.com>
2024-10-20 09:42:55 +0800

    [CHANGE] Removed the "Expand" button

--- src/components/NestedTable/NestedTable.tsx
+++ src/components/NestedTable/NestedTable.tsx
@@ -26,7 +26,6 @@ const generateColumnLabels = (numCols: number): string[] => {
 
 const NestedTable: React.FC<NestedTableProps> = ({ id, data }) => {
     const tableCtx = React.useContext(TableContext);
-    const [expanded, setExpanded] = React.useState<boolean>(false);
     const [hoveredCol, setHoveredCol] = React.useState<number | null>(null);
     const [hoveredRow, setHoveredRow] = React.useState<number | null>(null);
 
@@ -41,117 +40,111 @@ const NestedTable: React.FC<NestedTableProps> = ({ id, data }) => {
 
     return (
         <div style={{ margin: '20px' }}>
-            <Button variant="contained" onClick={() => setExpanded(!expanded)}>
-                {expanded ? 'Collapse' : 'Expand'}
-            </Button>
-
-            {expanded && (
-                <>
-                    <TableContainer component={Paper} style={{ marginTop: '20px' }}>
-                        <Table>
-                            <TableHead>
-                                <TableRow>
-                                    <TableCell /> {/* Empty cell for row numbers */}
-                                    {columnLabels.map((label, colIndex) => (
-                                        <TableCell
-                                            key={`col-header-${colIndex}`}
-                                            className={hoveredCol === colIndex ? styles['highlight-col'] : ''}
-                                            style={{ textAlign: 'center', fontWeight: 'bold' }}
+            <>
+                <TableContainer component={Paper} style={{ marginTop: '20px' }}>
+                    <Table>
+                        <TableHead>
+                            <TableRow>
+                                <TableCell /> {/* Empty cell for row numbers */}
+                                {columnLabels.map((label, colIndex) => (
+                                    <TableCell
+                                        key={`col-header-${colIndex}`}
+                                        className={hoveredCol === colIndex ? styles['highlight-col'] : ''}
+                                        style={{ textAlign: 'center', fontWeight: 'bold' }}
+                                    >
+                                        {label}
+                                    </TableCell>
+                                ))}
+                                <TableCell /> {/* Empty cell for delete column */}
+                            </TableRow>
+                        </TableHead>
+                        <TableBody>
+                            {/* Column Delete Row */}
+                            <TableRow>
+                                <TableCell /> {/* Empty cell for row numbers */}
+                                {data[0]?.map((_, colIndex) => (
+                                    <TableCell
+                                        key={`delete-col-${colIndex}`}
+                                        style={{ textAlign: 'center' }}
+                                        className={hoveredCol === colIndex ? styles['highlight-col'] : ''}
+                                    >
+                                        <IconButton
+                                            color="error"
+                                            onMouseEnter={() => setHoveredCol(colIndex)}
+                                            onMouseLeave={() => setHoveredCol(null)}
+                                            onClick={() => tableCtx.deleteColumn(id, colIndex)}
                                         >
-                                            {label}
+                                            <Delete />
+                                        </IconButton>
+                                    </TableCell>
+                                ))}
+                                <TableCell />
+                            </TableRow>
+
+                            {/* Table Rows */}
+                            {data.map((row, rowIndex) => (
+                                <TableRow
+                                    key={rowIndex}
+                                    className={hoveredRow === rowIndex ? styles['highlight-row'] : ''}
+                                >
+                                    {/* Row number */}
+                                    <TableCell
+                                        style={{ textAlign: 'center' }}
+                                        className={styles['row-number']}
+                                    >
+                                        {rowIndex + 1}
+                                    </TableCell>
+                                    {row.map((cell, colIndex) => (
+                                        <TableCell
+                                            key={colIndex}
+                                            className={
+                                                `${hoveredCol === colIndex ? styles['highlight-col'] : ''} ${isSelected(rowIndex, colIndex) ? styles['selected'] : ''}`
+                                            }
+                                            onClick={(e) =>
+                                                tableCtx.selectCell(id, rowIndex, colIndex, e.shiftKey, e.ctrlKey || e.metaKey)
+                                            }
+                                        >
+                                            <TextField
+                                                fullWidth
+                                                value={cell}
+                                                onChange={(e) =>
+                                                    isSelected(rowIndex, colIndex)
+                                                        ? tableCtx.updateSelectedCells(e.target.value)
+                                                        : tableCtx.updateCell(id, rowIndex, colIndex, e.target.value)
+                                                }
+                                                variant="outlined"
+                                                size="small"
+                                                autoComplete="off"
+                                            />
                                         </TableCell>
                                     ))}
-                                    <TableCell /> {/* Empty cell for delete column */}
-                                </TableRow>
-                            </TableHead>
-                            <TableBody>
-                                {/* Column Delete Row */}
-                                <TableRow>
-                                    <TableCell /> {/* Empty cell for row numbers */}
-                                    {data[0]?.map((_, colIndex) => (
-                                        <TableCell
-                                            key={`delete-col-${colIndex}`}
-                                            style={{ textAlign: 'center' }}
-                                            className={hoveredCol === colIndex ? styles['highlight-col'] : ''}
-                                        >
-                                            <IconButton
-                                                color="error"
-                                                onMouseEnter={() => setHoveredCol(colIndex)}
-                                                onMouseLeave={() => setHoveredCol(null)}
-                                                onClick={() => tableCtx.deleteColumn(id, colIndex)}
-                                            >
-                                                <Delete />
-                                            </IconButton>
-                                        </TableCell>
-                                    ))}
-                                    <TableCell />
-                                </TableRow>
-
-                                {/* Table Rows */}
-                                {data.map((row, rowIndex) => (
-                                    <TableRow
-                                        key={rowIndex}
+                                    <TableCell
                                         className={hoveredRow === rowIndex ? styles['highlight-row'] : ''}
                                     >
-                                        {/* Row number */}
-                                        <TableCell
-                                            style={{ textAlign: 'center' }}
-                                            className={styles['row-number']}
+                                        <IconButton
+                                            color="error"
+                                            onMouseEnter={() => setHoveredRow(rowIndex)}
+                                            onMouseLeave={() => setHoveredRow(null)}
+                                            onClick={() => tableCtx.deleteRow(id, rowIndex)}
                                         >
-                                            {rowIndex + 1}
-                                        </TableCell>
-                                        {row.map((cell, colIndex) => (
-                                            <TableCell
-                                                key={colIndex}
-                                                className={
-                                                    `${hoveredCol === colIndex ? styles['highlight-col'] : ''} ${isSelected(rowIndex, colIndex) ? styles['selected'] : ''}`
-                                                }
-                                                onClick={(e) =>
-                                                    tableCtx.selectCell(id, rowIndex, colIndex, e.shiftKey, e.ctrlKey || e.metaKey)
-                                                }
-                                            >
-                                                <TextField
-                                                    fullWidth
-                                                    value={cell}
-                                                    onChange={(e) =>
-                                                        isSelected(rowIndex, colIndex)
-                                                            ? tableCtx.updateSelectedCells(e.target.value)
-                                                            : tableCtx.updateCell(id, rowIndex, colIndex, e.target.value)
-                                                    }
-                                                    variant="outlined"
-                                                    size="small"
-                                                    autoComplete="off"
-                                                />
-                                            </TableCell>
-                                        ))}
-                                        <TableCell
-                                            className={hoveredRow === rowIndex ? styles['highlight-row'] : ''}
-                                        >
-                                            <IconButton
-                                                color="error"
-                                                onMouseEnter={() => setHoveredRow(rowIndex)}
-                                                onMouseLeave={() => setHoveredRow(null)}
-                                                onClick={() => tableCtx.deleteRow(id, rowIndex)}
-                                            >
-                                                <Delete />
-                                            </IconButton>
-                                        </TableCell>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
-                    </TableContainer>
+                                            <Delete />
+                                        </IconButton>
+                                    </TableCell>
+                                </TableRow>
+                            ))}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
 
-                    <div style={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
-                        <Button variant="contained" color="primary" onClick={() => tableCtx.addRow(id)}>
-                            Add Row
-                        </Button>
-                        <Button variant="contained" color="primary" onClick={() => tableCtx.addColumn(id)}>
-                            Add Column
-                        </Button>
-                    </div>
-                </>
-            )}
+                <div style={{ display: 'flex', gap: '10px', marginTop: '20px' }}>
+                    <Button variant="contained" color="primary" onClick={() => tableCtx.addRow(id)}>
+                        Add Row
+                    </Button>
+                    <Button variant="contained" color="primary" onClick={() => tableCtx.addColumn(id)}>
+                        Add Column
+                    </Button>
+                </div>
+            </>
         </div>
     );
 };
